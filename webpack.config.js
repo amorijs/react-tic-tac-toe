6c49788ede8a9b631309bc5ace3c005e89ff2bef
Chris Amori <chris.amori93@gmail.com>
2017-09-23 21:38:19 -0700

    Switch from live-server to webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       }
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
 
 // loader
