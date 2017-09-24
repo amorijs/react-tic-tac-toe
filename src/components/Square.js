84f1a05c744797485c890ca10c87cda7892fe13e
Chris Amori <chris.amori93@gmail.com>
2017-09-23 22:31:12 -0700

    Refactor scss to use variables, and add coloring for player turns

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Square = props => {
   const { handleClick, rowNum, squareNum, text } = props;
-  const squareClassName = `square ${text ? 'square-selected' : null}`;
+  const squareClassName = `square ${text ? `square-selected ${text}` : null}`;
 
   return (
     <div className={squareClassName} onClick={() => props.handleClick(rowNum, squareNum)}>
